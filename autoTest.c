ef863091d6cbd4a1d5f8969af0a9c2a7c694bff3
Nikki <46271636+nikkiguo@users.noreply.github.com>
2019-12-03 16:28:28 -0500

    Auto with sensors

--- autoTest.c
+++ autoTest.c
@@ -1,3 +1,5 @@
+#pragma config(Sensor, in1,    potentiometer,  sensorPotentiometer)
+#pragma config(Sensor, dgtl3,  bumperSwitch,   sensorTouch)
 #pragma config(Motor,  port1,           leftmotor,     tmotorVex269_HBridge, openLoop)
 #pragma config(Motor,  port9,           sideway,       tmotorVex269_MC29, openLoop)
 #pragma config(Motor,  port10,          rightmotor,    tmotorVex269_HBridge, openLoop)
@@ -8,13 +10,24 @@
 task main()
 {
 	while (1==1) {
-		motor[leftmotor]=63;
-		motor[rightmotor]=63;
-		wait1Msec(3000);
-		motor[leftmotor]=0;
-		motor[rightmotor]=0; //repeat
-		wait1Msec(3000);
+		while (SensorValue(bumperSwitch) == 1) {
+			motor[leftmotor]=20;
+			motor[rightmotor]=10;
+			wait1Msec(3000);
+
+			motor[leftmotor]=0;
+			motor[rightmotor]=0; //repeat
+			wait1Msec(3000);
+
+			while(SensorValue[potentiometer] < 500)
+			{
+				motor(sideway) = 5;
+			}
+			while(SensorValue[potentiometer] > 500)
+			{
+				motor(sideway) = -1;
+			}
+		}
+
 	}
-
-
 }
